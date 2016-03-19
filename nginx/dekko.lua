bab05866d97238c90e667d75b66e8830ac07c641
Павел Пронский <pavel@pronskiy.ru>
2016-03-19 14:13:27 +0000

    0.1.4 beta Added new features. Redis, Nginx, Lua.

--- nginx/dekko.lua
+++ nginx/dekko.lua
@@ -34,17 +34,18 @@ local args = ngx.req.get_uri_args();
 local red = redis:new();
 
 local redis_pool = {
-    "127.0.0.1:6379";
-    "127.0.0.2:6379";
-    "127.0.0.2:6379";
-    "127.0.0.3:6379";
+	"127.0.0.1:6379:password";
+	"127.0.0.2:6379";
+	"127.0.0.2:6379";
+	"127.0.0.3:6379";
 };
 
 -- code exceptions
 local codes = {
-    [404] = "Not found";
-    [400] = "Bad request";
-    [502] = "Redis not online";
+	[403] = "Redis auth failed";
+	[404] = "Not found";
+	[400] = "Bad request";
+	[502] = "Redis not online";
 };
 
 ngx.header["Content-Type"] = "application/json";
@@ -55,76 +56,84 @@ ngx.header["Access-Control-Allow-Credentials"] = "true";
 
 -- code exceptions construct
 local error_code = function(code)
-    local view = function(t)
-        ngx.say('{ status: '..code..', message: "' .. codes[code] .. '" }');
-        return ngx.exit(code);
-    end
-    return view(code);
+	local view = function(t)
+		ngx.say('{ status: '..code..', message: "' .. codes[code] .. '" }');
+		return ngx.exit(code);
+	end
+	return view(code);
 end
 
 -- check url vars
 if args.type == nil or args.domain == nil then
-    return error_code(400);
+	return error_code(400);
 end
 
 -- get keyhash modules
 local adverts = function(arg)
-    local data = {};
+	local data = {};
 	local n;
 	local hash = arg.domain .. ":" .. arg.type;
 
-    local hgetall, err = red:hgetall(hash)
-        if not hgetall then
-        return error_code(404);
-    end
+	local hgetall, err = red:hgetall(hash)
+		if not hgetall then
+		return error_code(404);
+	end
 
-    if #hgetall == 0 then
-        return error_code(404);
-    end
+	if #hgetall == 0 then
+		return error_code(404);
+	end
 
-    if type(hgetall) ~= "table" then
-        return error_code(400);
-    end
+	if type(hgetall) ~= "table" then
+		return error_code(400);
+	end
 
-    for i,v in pairs(hgetall) do
-        if i % 2 == 1 then n = v
-        else table.insert(data, ' { "' .. n .. '" : ' .. v .. ' } ') end
-    end
+	for i,v in pairs(hgetall) do
+		if i % 2 == 1 then n = v
+		else table.insert(data, ' { "' .. n .. '" : ' .. v .. ' } ') end
+	end
 
-    return ngx.say(string.format("[ %s ]", table.concat(data, ', ')))
+	return ngx.say(string.format("[ %s ]", table.concat(data, ', ')))
 end
 
 local redis_cluster = function(rp)
-    local target = false;
-    red:set_timeout(1000);
-    
-    -- math.randomseed(os.time());
-    -- host = split_host(rp[math.random(#rp)], ':');
-    
-    -- split ip and port
-    function split_host(s, d)
-        result = {};
-        for match in (s..d):gmatch("(.-)"..d) do
-            table.insert(result, match);
-        end
-        return result;
-    end
+	local target = false;
+	red:set_timeout(1000);
+	
+	-- math.randomseed(os.time());
+	-- host = split_host(rp[math.random(#rp)], ':');
+	
+	-- split ip and port
+	function split_host(s, d)
+		result = {};
+		for match in (s..d):gmatch("(.-)"..d) do
+			table.insert(result, match);
+		end
+		return result;
+	end
 
-    -- check ip port
-    for i, host in pairs(rp) do
-        local s = split_host(host, ':');
-        local con, err = red:connect(s[1], s[2]);
-            if con then target = true;
-            return adverts(args);
-        end
-    end
+	-- check ip port
+	for i, host in pairs(rp) do
+		local s = split_host(host, ':');
+		local con, err = red:connect(s[1], s[2]);
+			if con then target = true;
 
-    -- host not online
-    if not target then
-        return rcodes(error_code);
-    end
+			if s[3] then
+				local res, err = red:auth(s[3])
+				if not res then
+					return error_code(403);
+				end
+			end
 
-    red:close();
+			return adverts(args);
+		end
+	end
+
+	-- host not online
+	if not target then
+		return rcodes(error_code);
+	end
+
+	red:close();
 end
 
 redis_cluster(redis_pool);
