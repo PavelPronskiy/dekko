23a17b40e0406936d3858553ef7ad5cc1fedee2a
Павел Пронский <pavel@pronskiy.ru>
2016-03-20 20:39:03 +0000

    Added client browser fingerprint

--- dekko/dekko.js
+++ dekko/dekko.js
@@ -427,7 +427,7 @@
 				ajax.data 				= {},
 				ajax.data.domain 		= window.location.hostname || window.location.host,
 				ajax.data.type 			= o.type,
-				ajax.data.click			= true,
+				ajax.data.click			= this.fingerprint(),
 				ajax.data.module		= o.name,
 				ajax.crossDomain 		= false,
 
@@ -442,8 +442,28 @@
 						return false;
 					
 				};
-				// return console.log(ajax);
+				
+				
+				// return console.log();
 				return this.ajax(ajax);
+			},
+			fingerprint: function() {
+				return $.md5([
+					navigator.userAgent,
+					[ screen.height, screen.width, screen.colorDepth ].join("x"),
+					( new Date() ).getTimezoneOffset(),
+					!!window.sessionStorage,
+					!!window.localStorage,
+					$.map( navigator.plugins, function(p) {
+						return [
+							p.name,
+							p.description,
+							$.map( p, function(mt) {
+								return [ mt.type, mt.suffixes ].join("~");
+							}).join(",")
+						].join("::");
+					}).join(";")
+					].join("###"));
 			},
 		};
 
@@ -525,3 +545,25 @@
  * Date: @DATE
  */
 !function(r,n){"function"==typeof define&&define.amd?define(["jquery"],n):"object"==typeof exports?module.exports=n(require("jquery")):n(r.jQuery)}(this,function(r,n){function t(r,n,t){var e=c[n.type]||{};return null==r?t||!n.def?null:n.def:(r=e.floor?~~r:parseFloat(r),isNaN(r)?n.def:e.mod?(r+e.mod)%e.mod:0>r?0:e.max<r?e.max:r)}function e(n){var t=l(),e=t._rgba=[];return n=n.toLowerCase(),h(u,function(r,o){var a,i=o.re.exec(n),s=i&&o.parse(i),u=o.space||"rgba";return s?(a=t[u](s),t[f[u].cache]=a[f[u].cache],e=t._rgba=a._rgba,!1):void 0}),e.length?("0,0,0,0"===e.join()&&r.extend(e,a.transparent),t):a[n]}function o(r,n,t){return t=(t+1)%1,1>6*t?r+(n-r)*t*6:1>2*t?n:2>3*t?r+(n-r)*(2/3-t)*6:r}var a,i="backgroundColor borderBottomColor borderLeftColor borderRightColor borderTopColor color columnRuleColor outlineColor textDecorationColor textEmphasisColor",s=/^([\-+])=\s*(\d+\.?\d*)/,u=[{re:/rgba?\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*(?:,\s*(\d?(?:\.\d+)?)\s*)?\)/,parse:function(r){return[r[1],r[2],r[3],r[4]]}},{re:/rgba?\(\s*(\d+(?:\.\d+)?)\%\s*,\s*(\d+(?:\.\d+)?)\%\s*,\s*(\d+(?:\.\d+)?)\%\s*(?:,\s*(\d?(?:\.\d+)?)\s*)?\)/,parse:function(r){return[2.55*r[1],2.55*r[2],2.55*r[3],r[4]]}},{re:/#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})/,parse:function(r){return[parseInt(r[1],16),parseInt(r[2],16),parseInt(r[3],16)]}},{re:/#([a-f0-9])([a-f0-9])([a-f0-9])/,parse:function(r){return[parseInt(r[1]+r[1],16),parseInt(r[2]+r[2],16),parseInt(r[3]+r[3],16)]}},{re:/hsla?\(\s*(\d+(?:\.\d+)?)\s*,\s*(\d+(?:\.\d+)?)\%\s*,\s*(\d+(?:\.\d+)?)\%\s*(?:,\s*(\d?(?:\.\d+)?)\s*)?\)/,space:"hsla",parse:function(r){return[r[1],r[2]/100,r[3]/100,r[4]]}}],l=r.Color=function(n,t,e,o){return new r.Color.fn.parse(n,t,e,o)},f={rgba:{props:{red:{idx:0,type:"byte"},green:{idx:1,type:"byte"},blue:{idx:2,type:"byte"}}},hsla:{props:{hue:{idx:0,type:"degrees"},saturation:{idx:1,type:"percent"},lightness:{idx:2,type:"percent"}}}},c={"byte":{floor:!0,max:255},percent:{max:1},degrees:{mod:360,floor:!0}},p=l.support={},d=r("<p>")[0],h=r.each;d.style.cssText="background-color:rgba(1,1,1,.5)",p.rgba=d.style.backgroundColor.indexOf("rgba")>-1,h(f,function(r,n){n.cache="_"+r,n.props.alpha={idx:3,type:"percent",def:1}}),l.fn=r.extend(l.prototype,{parse:function(o,i,s,u){if(o===n)return this._rgba=[null,null,null,null],this;(o.jquery||o.nodeType)&&(o=r(o).css(i),i=n);var c=this,p=r.type(o),d=this._rgba=[];return i!==n&&(o=[o,i,s,u],p="array"),"string"===p?this.parse(e(o)||a._default):"array"===p?(h(f.rgba.props,function(r,n){d[n.idx]=t(o[n.idx],n)}),this):"object"===p?(o instanceof l?h(f,function(r,n){o[n.cache]&&(c[n.cache]=o[n.cache].slice())}):h(f,function(n,e){var a=e.cache;h(e.props,function(r,n){if(!c[a]&&e.to){if("alpha"===r||null==o[r])return;c[a]=e.to(c._rgba)}c[a][n.idx]=t(o[r],n,!0)}),c[a]&&r.inArray(null,c[a].slice(0,3))<0&&(c[a][3]=1,e.from&&(c._rgba=e.from(c[a])))}),this):void 0},is:function(r){var n=l(r),t=!0,e=this;return h(f,function(r,o){var a,i=n[o.cache];return i&&(a=e[o.cache]||o.to&&o.to(e._rgba)||[],h(o.props,function(r,n){return null!=i[n.idx]?t=i[n.idx]===a[n.idx]:void 0})),t}),t},_space:function(){var r=[],n=this;return h(f,function(t,e){n[e.cache]&&r.push(t)}),r.pop()},transition:function(r,n){var e=l(r),o=e._space(),a=f[o],i=0===this.alpha()?l("transparent"):this,s=i[a.cache]||a.to(i._rgba),u=s.slice();return e=e[a.cache],h(a.props,function(r,o){var a=o.idx,i=s[a],l=e[a],f=c[o.type]||{};null!==l&&(null===i?u[a]=l:(f.mod&&(l-i>f.mod/2?i+=f.mod:i-l>f.mod/2&&(i-=f.mod)),u[a]=t((l-i)*n+i,o)))}),this[o](u)},blend:function(n){if(1===this._rgba[3])return this;var t=this._rgba.slice(),e=t.pop(),o=l(n)._rgba;return l(r.map(t,function(r,n){return(1-e)*o[n]+e*r}))},toRgbaString:function(){var n="rgba(",t=r.map(this._rgba,function(r,n){return null==r?n>2?1:0:r});return 1===t[3]&&(t.pop(),n="rgb("),n+t.join()+")"},toHslaString:function(){var n="hsla(",t=r.map(this.hsla(),function(r,n){return null==r&&(r=n>2?1:0),n&&3>n&&(r=Math.round(100*r)+"%"),r});return 1===t[3]&&(t.pop(),n="hsl("),n+t.join()+")"},toHexString:function(n){var t=this._rgba.slice(),e=t.pop();return n&&t.push(~~(255*e)),"#"+r.map(t,function(r){return r=(r||0).toString(16),1===r.length?"0"+r:r}).join("")},toString:function(){return 0===this._rgba[3]?"transparent":this.toRgbaString()}}),l.fn.parse.prototype=l.fn,f.hsla.to=function(r){if(null==r[0]||null==r[1]||null==r[2])return[null,null,null,r[3]];var n,t,e=r[0]/255,o=r[1]/255,a=r[2]/255,i=r[3],s=Math.max(e,o,a),u=Math.min(e,o,a),l=s-u,f=s+u,c=.5*f;return n=u===s?0:e===s?60*(o-a)/l+360:o===s?60*(a-e)/l+120:60*(e-o)/l+240,t=0===l?0:.5>=c?l/f:l/(2-f),[Math.round(n)%360,t,c,null==i?1:i]},f.hsla.from=function(r){if(null==r[0]||null==r[1]||null==r[2])return[null,null,null,r[3]];var n=r[0]/360,t=r[1],e=r[2],a=r[3],i=.5>=e?e*(1+t):e+t-e*t,s=2*e-i;return[Math.round(255*o(s,i,n+1/3)),Math.round(255*o(s,i,n)),Math.round(255*o(s,i,n-1/3)),a]},h(f,function(e,o){var a=o.props,i=o.cache,u=o.to,f=o.from;l.fn[e]=function(e){if(u&&!this[i]&&(this[i]=u(this._rgba)),e===n)return this[i].slice();var o,s=r.type(e),c="array"===s||"object"===s?e:arguments,p=this[i].slice();return h(a,function(r,n){var e=c["object"===s?r:n.idx];null==e&&(e=p[n.idx]),p[n.idx]=t(e,n)}),f?(o=l(f(p)),o[i]=p,o):l(p)},h(a,function(n,t){l.fn[n]||(l.fn[n]=function(o){var a,i=r.type(o),u="alpha"===n?this._hsla?"hsla":"rgba":e,l=this[u](),f=l[t.idx];return"undefined"===i?f:("function"===i&&(o=o.call(this,f),i=r.type(o)),null==o&&t.empty?this:("string"===i&&(a=s.exec(o),a&&(o=f+parseFloat(a[2])*("+"===a[1]?1:-1))),l[t.idx]=o,this[u](l)))})})}),l.hook=function(n){var t=n.split(" ");h(t,function(n,t){r.cssHooks[t]={set:function(n,o){var a,i,s="";if("transparent"!==o&&("string"!==r.type(o)||(a=e(o)))){if(o=l(a||o),!p.rgba&&1!==o._rgba[3]){for(i="backgroundColor"===t?n.parentNode:n;(""===s||"transparent"===s)&&i&&i.style;)try{s=r.css(i,"backgroundColor"),i=i.parentNode}catch(u){}o=o.blend(s&&"transparent"!==s?s:"_default")}o=o.toRgbaString()}try{n.style[t]=o}catch(u){}}},r.fx.step[t]=function(n){n.colorInit||(n.start=l(n.elem,t),n.end=l(n.end),n.colorInit=!0),r.cssHooks[t].set(n.elem,n.start.transition(n.end,n.pos))}})},l.hook(i),r.cssHooks.borderColor={expand:function(r){var n={};return h(["Top","Right","Bottom","Left"],function(t,e){n["border"+e+"Color"]=r}),n}},a=r.Color.names={aqua:"#00ffff",black:"#000000",blue:"#0000ff",fuchsia:"#ff00ff",gray:"#808080",green:"#008000",lime:"#00ff00",maroon:"#800000",navy:"#000080",olive:"#808000",purple:"#800080",red:"#ff0000",silver:"#c0c0c0",teal:"#008080",white:"#ffffff",yellow:"#ffff00",transparent:[null,null,null,0],_default:"#ffffff"}});
+
+
+
+/*
+ * jQuery MD5 Plugin 1.2.1
+ * https://github.com/blueimp/jQuery-MD5
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://creativecommons.org/licenses/MIT/
+ * 
+ * Based on
+ * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message
+ * Digest Algorithm, as defined in RFC 1321.
+ * Version 2.2 Copyright (C) Paul Johnston 1999 - 2009
+ * Other contributors: Greg Holt, Andrew Kepert, Ydnar, Lostinet
+ * Distributed under the BSD License
+ * See http://pajhome.org.uk/crypt/md5 for more info.
+ */
+!function(n){"use strict";function t(n,t){var r=(65535&n)+(65535&t),u=(n>>16)+(t>>16)+(r>>16);return u<<16|65535&r}function r(n,t){return n<<t|n>>>32-t}function u(n,u,e,o,c,f){return t(r(t(t(u,n),t(o,f)),c),e)}function e(n,t,r,e,o,c,f){return u(t&r|~t&e,n,t,o,c,f)}function o(n,t,r,e,o,c,f){return u(t&e|r&~e,n,t,o,c,f)}function c(n,t,r,e,o,c,f){return u(t^r^e,n,t,o,c,f)}function f(n,t,r,e,o,c,f){return u(r^(t|~e),n,t,o,c,f)}function i(n,r){n[r>>5]|=128<<r%32,n[(r+64>>>9<<4)+14]=r;var u,i,h,a,g,l=1732584193,d=-271733879,v=-1732584194,C=271733878;for(u=0;u<n.length;u+=16)i=l,h=d,a=v,g=C,l=e(l,d,v,C,n[u],7,-680876936),C=e(C,l,d,v,n[u+1],12,-389564586),v=e(v,C,l,d,n[u+2],17,606105819),d=e(d,v,C,l,n[u+3],22,-1044525330),l=e(l,d,v,C,n[u+4],7,-176418897),C=e(C,l,d,v,n[u+5],12,1200080426),v=e(v,C,l,d,n[u+6],17,-1473231341),d=e(d,v,C,l,n[u+7],22,-45705983),l=e(l,d,v,C,n[u+8],7,1770035416),C=e(C,l,d,v,n[u+9],12,-1958414417),v=e(v,C,l,d,n[u+10],17,-42063),d=e(d,v,C,l,n[u+11],22,-1990404162),l=e(l,d,v,C,n[u+12],7,1804603682),C=e(C,l,d,v,n[u+13],12,-40341101),v=e(v,C,l,d,n[u+14],17,-1502002290),d=e(d,v,C,l,n[u+15],22,1236535329),l=o(l,d,v,C,n[u+1],5,-165796510),C=o(C,l,d,v,n[u+6],9,-1069501632),v=o(v,C,l,d,n[u+11],14,643717713),d=o(d,v,C,l,n[u],20,-373897302),l=o(l,d,v,C,n[u+5],5,-701558691),C=o(C,l,d,v,n[u+10],9,38016083),v=o(v,C,l,d,n[u+15],14,-660478335),d=o(d,v,C,l,n[u+4],20,-405537848),l=o(l,d,v,C,n[u+9],5,568446438),C=o(C,l,d,v,n[u+14],9,-1019803690),v=o(v,C,l,d,n[u+3],14,-187363961),d=o(d,v,C,l,n[u+8],20,1163531501),l=o(l,d,v,C,n[u+13],5,-1444681467),C=o(C,l,d,v,n[u+2],9,-51403784),v=o(v,C,l,d,n[u+7],14,1735328473),d=o(d,v,C,l,n[u+12],20,-1926607734),l=c(l,d,v,C,n[u+5],4,-378558),C=c(C,l,d,v,n[u+8],11,-2022574463),v=c(v,C,l,d,n[u+11],16,1839030562),d=c(d,v,C,l,n[u+14],23,-35309556),l=c(l,d,v,C,n[u+1],4,-1530992060),C=c(C,l,d,v,n[u+4],11,1272893353),v=c(v,C,l,d,n[u+7],16,-155497632),d=c(d,v,C,l,n[u+10],23,-1094730640),l=c(l,d,v,C,n[u+13],4,681279174),C=c(C,l,d,v,n[u],11,-358537222),v=c(v,C,l,d,n[u+3],16,-722521979),d=c(d,v,C,l,n[u+6],23,76029189),l=c(l,d,v,C,n[u+9],4,-640364487),C=c(C,l,d,v,n[u+12],11,-421815835),v=c(v,C,l,d,n[u+15],16,530742520),d=c(d,v,C,l,n[u+2],23,-995338651),l=f(l,d,v,C,n[u],6,-198630844),C=f(C,l,d,v,n[u+7],10,1126891415),v=f(v,C,l,d,n[u+14],15,-1416354905),d=f(d,v,C,l,n[u+5],21,-57434055),l=f(l,d,v,C,n[u+12],6,1700485571),C=f(C,l,d,v,n[u+3],10,-1894986606),v=f(v,C,l,d,n[u+10],15,-1051523),d=f(d,v,C,l,n[u+1],21,-2054922799),l=f(l,d,v,C,n[u+8],6,1873313359),C=f(C,l,d,v,n[u+15],10,-30611744),v=f(v,C,l,d,n[u+6],15,-1560198380),d=f(d,v,C,l,n[u+13],21,1309151649),l=f(l,d,v,C,n[u+4],6,-145523070),C=f(C,l,d,v,n[u+11],10,-1120210379),v=f(v,C,l,d,n[u+2],15,718787259),d=f(d,v,C,l,n[u+9],21,-343485551),l=t(l,i),d=t(d,h),v=t(v,a),C=t(C,g);return[l,d,v,C]}function h(n){var t,r="";for(t=0;t<32*n.length;t+=8)r+=String.fromCharCode(n[t>>5]>>>t%32&255);return r}function a(n){var t,r=[];for(r[(n.length>>2)-1]=void 0,t=0;t<r.length;t+=1)r[t]=0;for(t=0;t<8*n.length;t+=8)r[t>>5]|=(255&n.charCodeAt(t/8))<<t%32;return r}function g(n){return h(i(a(n),8*n.length))}function l(n,t){var r,u,e=a(n),o=[],c=[];for(o[15]=c[15]=void 0,e.length>16&&(e=i(e,8*n.length)),r=0;16>r;r+=1)o[r]=909522486^e[r],c[r]=1549556828^e[r];return u=i(o.concat(a(t)),512+8*t.length),h(i(c.concat(u),640))}function d(n){var t,r,u="0123456789abcdef",e="";for(r=0;r<n.length;r+=1)t=n.charCodeAt(r),e+=u.charAt(t>>>4&15)+u.charAt(15&t);return e}function v(n){return unescape(encodeURIComponent(n))}function C(n){return g(v(n))}function s(n){return d(C(n))}function A(n,t){return l(v(n),v(t))}function m(n,t){return d(A(n,t))}n.md5=function(n,t,r){return t?r?A(t,n):m(t,n):r?C(n):s(n)}}("function"==typeof jQuery?jQuery:this);
--- nginx/dekko.lua
+++ nginx/dekko.lua
@@ -29,73 +29,73 @@
 -- OTHER DEALINGS IN THE SOFTWARE.
 
 
-local redis = require "resty.redis";
-local args = ngx.req.get_uri_args();
-local red = redis:new();
-local dekko = {};
-dekko.ngx = {};
-dekko.redis = {};
-dekko.construct = {};
-dekko.redis.timeout = 1000;
+local redis = require "resty.redis"
+local args = ngx.req.get_uri_args()
+local red = redis:new()
+local dekko = {}
+dekko.ngx = {}
+dekko.redis = {}
+dekko.construct = {}
+dekko.redis.timeout = 1000
 
 dekko.redis.pool = {
-	"127.0.0.2:6379:passwd",
 	"127.0.0.1:6379",
-	"127.0.0.2:6379",
-	"127.0.0.3:6379"
-};
+	"127.0.0.2:6379:passwd",
+	"127.0.0.3:6379",
+	"127.0.0.4:6379",
+}
 
 dekko.redis.prefix = {
     ["modules"] = "modules",
     ["stat"] = "stat",
     ["clicks"] = "clicks",
-    ["hosts"] = "hosts"
-};
+    ["hosts"] = "hosts",
+}
 
 dekko.redis.codes = {
 	[200] = "OK",
 	[403] = "Redis auth failed",
 	[404] = "Not found",
 	[400] = "Bad request",
-	[502] = "Redis not online"
-};
+	[502] = "Redis not online",
+}
 
 dekko.ngx.headers = function()
-	ngx.header["Content-Type"] = "application/json";
-	ngx.header["Access-Control-Allow-Origin"] = '*';
-	ngx.header["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, DELETE, PUT";
-	ngx.header["Access-Control-Allow-Headers"] = "x-requested-with, Content-Type, origin, authorization, accept, client-security-token";
-	ngx.header["Access-Control-Allow-Credentials"] = "true";
+	ngx.header["Content-Type"] = "application/json"
+	ngx.header["Access-Control-Allow-Origin"] = '*'
+	ngx.header["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, DELETE, PUT"
+	ngx.header["Access-Control-Allow-Headers"] = "x-requested-with, Content-Type, origin, authorization, accept, client-security-token"
+	ngx.header["Access-Control-Allow-Credentials"] = "true"
 end
 
 dekko.ngx.exception = function(c)
-    ngx.say('{ "status": ' .. c .. ', "message": "' .. dekko.redis.codes[c] .. '" }');
-    return ngx.exit(c);
+    ngx.say('{ "status": ' .. c .. ', "message": "' .. dekko.redis.codes[c] .. '" }')
+    return ngx.exit(c)
 end
 
 dekko.splitHost = function(s,d)
 	local result = {};
 	for match in (s..d):gmatch("(.-)"..d) do
-		table.insert(result, match);
+		table.insert(result, match)
 	end
-	return result;
+	return result
 end
 
 dekko.redis.hgetall = function(hash)
 	local hgetall, err = red:hgetall(hash)
 		if not hgetall then
-		return dekko.ngx.exception(404);
+		return dekko.ngx.exception(404)
 	end
 
 	if #hgetall == 0 then
-		return dekko.ngx.exception(404);
+		return dekko.ngx.exception(404)
 	end
 
 	if type(hgetall) ~= "table" then
-		return dekko.ngx.exception(400);
+		return dekko.ngx.exception(400)
 	end
 
-	return hgetall;
+	return hgetall
 	
 end
 
@@ -115,31 +115,30 @@ dekko.construct.clicks = function(obj)
 	
 	-- local res, err = red:hmset("myhash", "field1", "Hello", "field2", "World")
 	-- if not res
-	
+	local remote_addr = ngx.var.remote_addr .. ':' .. obj.fingerprint
 	red:init_pipeline()
 	red:hincrby(obj.hash.clicks, args.module, 1)
-	red:hmset(obj.hash.hosts, ngx.var.remote_addr, args.module)
+	red:hmset(obj.hash.hosts, remote_addr, args.module)
 
 	local results, err = red:commit_pipeline()
 	if not results then
-		return dekko.ngx.exception(400);
+		return dekko.ngx.exception(400)
 	end
 	
 	-- ngx.say(obj.hash.clicks);
-	return dekko.ngx.exception(200);
+	return dekko.ngx.exception(200)
 end
 
 -- route objects
 function dekko:router(obj)
 
     if obj.route == 'adverts' then
-        return self.construct.adverts(obj.hash);
+        return self.construct.adverts(obj.hash)
     end
 
     if obj.route == 'click' then
-        return self.construct.clicks(obj);
+        return self.construct.clicks(obj)
     end
-
 
 end
 
@@ -151,52 +150,55 @@ function dekko:connect(obj)
 	-- host = split_host(rp[math.random(#rp)], ':');
     -- local hash = arg.domain .. redis_hash_modules_prefix .. arg.type;
 
-	red:set_timeout(dekko.redis.timeout);
+	red:set_timeout(dekko.redis.timeout)
 
 	-- check ip port
 	for i, host in pairs(dekko.redis.pool) do
-		local s = dekko.splitHost(host, ':');
-		local con, err = red:connect(s[1], s[2]);
+		local s = dekko.splitHost(host, ':')
+		local con, err = red:connect(s[1], s[2])
 			if con then target = true;
 			if s[3] then
 				local res, err = red:auth(s[3])
 				if not res then
-					return dekko.ngx.exception(403);
+					return dekko.ngx.exception(403)
 				end
 			end
-			return self:router(obj);
+			return self:router(obj)
 		end
 	end
 
 	-- host not online
 	if not target then
-		return dekko.ngx.exception(502);
+		return dekko.ngx.exception(502)
 	end
 
-	red:close();
+	red:set_keepalive(10000, 100)
+
+	-- red:close();
 end
 
 
 
 -- check advert params
 function dekko:init()
-    local obj = {};
+    local obj = {}
     
-    dekko.ngx.headers();
+    dekko.ngx.headers()
     if args.type ~= nil and args.domain ~= nil and args.click == nil then
-        obj.route = 'adverts';
-        obj.hash = args.domain .. ':' .. dekko.redis.prefix.modules .. ':' .. args.type;
+        obj.route = 'adverts'
+        obj.hash = args.domain .. ':' .. dekko.redis.prefix.modules .. ':' .. args.type
     elseif args.type ~= nil and args.domain ~= nil and args.click ~= nil and args.module ~= nil then
-        obj.hash = {};
-        obj.route = 'click';
-        obj.module = args.module;
-        obj.hash.clicks = args.domain .. ':' .. dekko.redis.prefix.stat .. ':' .. dekko.redis.prefix.clicks;
-        obj.hash.hosts = args.domain .. ':' .. dekko.redis.prefix.stat .. ':' .. dekko.redis.prefix.hosts;
+        obj.hash = {}
+        obj.route = 'click'
+        obj.module = args.module
+        obj.fingerprint = args.click
+        obj.hash.clicks = args.domain .. ':' .. dekko.redis.prefix.stat .. ':' .. dekko.redis.prefix.clicks
+        obj.hash.hosts = args.domain .. ':' .. dekko.redis.prefix.stat .. ':' .. dekko.redis.prefix.hosts
     else
-    	return dekko.ngx.exception(400);
+    	return dekko.ngx.exception(400)
     end
 
-    return dekko:connect(obj);
+    return dekko:connect(obj)
 end
 
-dekko:init();
+dekko:init()
